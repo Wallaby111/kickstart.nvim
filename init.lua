56b0149fd9ae450d39dfc6206187a4056732e8ca
Morgan Hester <morganhasebehester@gmail.com>
2024-03-09 15:57:05 -0800

    Added python lsp and comments for bash, docker, and yaml, but they will not install. Commented until I can figure out.

--- init.lua
+++ init.lua
@@ -534,9 +534,12 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
-        -- pyright = {},
+        pylsp = {},
         rust_analyzer = {},
         ocamllsp = {},
+        -- bashls = {},
+        -- dockerls = {},
+        -- yamlls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
